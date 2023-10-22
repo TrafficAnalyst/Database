6d628502917448e136895c4804d4b8a7c1a1bb9d
nafilarossiana <nafilarossiana@gmail.com>
2023-10-22 19:47:45 +0700

    update

--- API-traffic/api-service.py
+++ API-traffic/api-service.py
@@ -3,30 +3,31 @@ import requests
 
 app = Flask(__name__)
 
-# Fungsi untuk mengambil data dari API database
-def get_data_from_database():
-    # Gantilah URL dengan URL API database Anda
-    database_url = ""
-    response = requests.get(database_url)
-    data = response.json()
-    return data
-
 # Fungsi untuk melakukan prediksi menggunakan API machine learning
 def predict_using_ml(data):
     # Gantilah URL dengan URL API machine learning Anda
-    ml_url = ""
+    ml_url = "https://csbnpmnk-5000.asse.devtunnels.ms/"
     response = requests.post(ml_url, json=data)
     prediction = response.json()
     return prediction
 
-@app.route('/get-deteksi', methods=['POST'])
+# Fungsi untuk mengirim data ke API database
+def send_data_to_database(data):
+    # Gantilah URL dengan URL API database Anda yang menerima data melalui POST
+    database_url = "https://csbnpmnk-8080.asse.devtunnels.ms/"
+    response = requests.post(database_url, json=data)
+    return response.json()
+
+@app.route('/get-prediction', methods=['POST'])
 def get_prediction():
     try:
         # Mengambil data dari API database
-        database_data = get_data_from_database()
+        
+        data_to_send = {"key1": "value1", "key2": "value2"}  # Gantilah dengan data yang sesuai
+        send_data_to_database(data_to_send)
 
         # Melakukan prediksi menggunakan API machine learning
-        prediction = predict_using_ml(database_data)
+        prediction = predict_using_ml(data_to_send)
 
         return jsonify({"prediction": prediction}), 200
     except Exception as e:
